8bca1f1d697f80d35d69804069bcd5d0f129cfd3
iamfiham <xyzfiham@gmail.com>
2024-04-25 23:20:26 +0530

    bug fix

--- src/Componants/CoverImage.jsx
+++ src/Componants/CoverImage.jsx
@@ -1,8 +1,8 @@
-import Banner from "../assets/banner.webp";
-import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import "./assets/Components.scss";
+import Banner from '../assets/banner.webp';
+import {useRef} from 'react';
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+import './assets/Components.scss';
 // import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function CoverImage() {
@@ -13,20 +13,20 @@ export function CoverImage() {
       coverImage.current,
       {
         scale: 2,
-        clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+        clipPath: 'polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)',
       },
       {
         scale: 1,
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
         duration: 1,
-        ease: "power4.out",
+        ease: 'power4.out',
       }
     );
   }, {});
 
   return (
-    <div className="cover-image">
-      <img src={Banner} alt="banner" ref={coverImage} />
+    <div className='cover-image'>
+      <img src={Banner} alt='banner' ref={coverImage} />
       {/* <LazyLoadImage
         src={Banner}
         alt="banner"
