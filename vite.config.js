6d04b671a39ec95384014b7d75dc8a73d021c1c6
iamfiham <xyzfiham@gmail.com>
2024-06-04 21:34:34 +0530

    website url change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/online-cv/',
+  base: '',
   css: {
     postcss: {
       plugins: [tailwindcss()],
